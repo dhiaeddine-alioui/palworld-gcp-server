279bf131804ce5d66765b0ccf08f38647c57a740
Dhiaeddine Alioui <dhiaeddine.alioui@gmail.com>
2024-02-11 11:46:01 +0100

    Create a new user

--- creation_script.sh
+++ creation_script.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# Define username and password
+USERNAME="serveruser"
+PASSWORD="serveruser"
+
+# Create the user with bash as default shell
+sudo useradd -m -s /bin/bash $USERNAME
+
+# Set the password for the user
+echo -e "$PASSWORD\n$PASSWORD" | sudo passwd $USERNAME
+
+# Check if user creation and password setup were successful
+if [ $? -eq 0 ]; then
+    echo "User $USERNAME created successfully with password $PASSWORD"
+    # Switch to the newly created user
+    sudo su - $USERNAME
+
+else
+    echo "Failed to create user or set password"
+fi
+
